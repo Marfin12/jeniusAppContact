22ba216f4a18d1f205d112f50ffdf3856eff3700
Marfin Fadhilah <fadilahmarfin@gmail.com>
2021-12-11 17:49:25 +0700

    chore(setup-ui-design): [src-constants] Add screen route name & title

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -6,6 +6,20 @@ const Constants = {
   UTILS: {
     EMPTY_FUNCTION: () => {},
   },
+  SCREEN: {
+    CONTACT: {
+      ROUTE_NAME: 'CONTACT_SCREEN',
+      TITLE: 'Contact List'
+    },
+    PROFILE: {
+      ROUTE_NAME: 'PROFILE_SCREEN',
+      TITLE: 'Contact'
+    },
+    EDIT: {
+      ROUTE_NAME: 'EDIT_SCREEN',
+      TITLE: 'Edit Contact'
+    }
+  }
 };
 
 export default Constants;
